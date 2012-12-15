a9d44fa69384e99c7ca76c89b917ce1681123c29
Rodrigo Silveira <rsilveira@gmail.com>
2012-12-15 16:45:44 +0100

    Made filtering of points (when creating a polgon_2) optional

--- cpp/rb_Polygon_2.cpp
+++ cpp/rb_Polygon_2.cpp
@@ -1,21 +1,53 @@
 #include "rb_Polygon_2.h"
 
-Polygon_2 build(Array points){
+const double MIN_DIFF = 0.0001; // Difference in coordinates to consider two consecutive points too close
+
+/*
+ * Internal use only (use build/build_after_filtering instead)
+ * Builds a new Polygon_2 with the points specified in points
+ * If filter=true, it filters consecutive points that are too close to each other
+ */
+Polygon_2 build_polygon(Array points, bool filter=false){
 	Polygon_2 polygon;
+	double prevX = 0, prevY = 0;
 	for (unsigned int i = 0; i < points.size(); i++) {
 		Array coordinate = Array(points[i]);
-		Point_2 point(
-				from_ruby<double>(coordinate[0]),
-				from_ruby<double>(coordinate[1]));
-		polygon.push_back(point);
+		double x = from_ruby<double>(coordinate[0]);
+		double y = from_ruby<double>(coordinate[1]);
+		double diffX = x-prevX;
+		double diffY = y-prevY;
+		// If filtering: skip point if difference is too small (can produce a degenate segment)
+		if (!filter || (diffX*diffX > MIN_DIFF*MIN_DIFF || diffY*diffY > MIN_DIFF*MIN_DIFF)) { 
+		  Point_2 point(x,y);
+		  polygon.push_back(point);
+		}
+		prevX = x;
+		prevY = y;
 	}
 	return polygon;
+}
+
+/*
+ * Same as build() but filtering consecutive points that are too close between each other
+ */
+Polygon_2 build_after_filtering(Array points){
+	
+	return build_polygon(points, true);
+}
+
+
+/*
+ * Builds a new Polygon_2 with akk the points specified in points
+ *
+ */
+Polygon_2 build(Array points){
+	return  build_polygon(points, false);
 }
 
 Array to_a(Polygon_2 polygon){
 	Array points;
 	Polygon_2::Vertex_iterator vit;
-  for (vit = polygon.vertices_begin(); vit != polygon.vertices_end(); ++vit){
+	for (vit = polygon.vertices_begin(); vit != polygon.vertices_end(); ++vit){
 		Point_2 point = *vit;
 		points.push(point);
 	}
@@ -27,6 +59,7 @@ Data_Type<Polygon_2> define_Polygon_2(Rice::Module rb_mCGAL ) {
 	Data_Type<Polygon_2> rb_cPolygon_2 =
 		define_class_under<Polygon_2>(rb_mCGAL, "Polygon_2")
 		.define_singleton_method("build", &build)
+		.define_singleton_method("build_after_filtering", &build_after_filtering)
 		.define_method("simple?", &Polygon_2::is_simple)
 		.define_method("clockwise?", &Polygon_2::is_clockwise_oriented)
 		.define_method("counterclockwise?", &Polygon_2::is_counterclockwise_oriented)
